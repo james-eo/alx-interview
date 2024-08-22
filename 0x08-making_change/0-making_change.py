2ef9c5264d2f82d380171f80542b6637b7a09d7d
james-eo <jameseokonkwo@gmail.com>
2024-08-22 14:52:40 +0100

    Updated 0-making_changes.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -17,18 +17,18 @@ def makeChange(coins, total):
         The fewest number of coins needed to meet the total,
         or -1 if it's impossible.
     """
+    coins.sort(reverse=True)
+    coin_count = 0
+
     if total <= 0:
         return 0
 
-    dp = [total + 1] * (total + 1)
-    dp[0] = 0
+    for coin in coins:
+        if total >= coin:
+            coin_count += total // coin
+            total %= coin
 
-    coins.sort(reverse=True)
+        if total == 0:
+            return coin_count
 
-    for amount in range(1, total + 1):
-        for coin in coins:
-            if coin > amount:
-                continue
-            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
-
-    return dp[total] if dp[total] != total + 1 else -1
+    return -1
